520bdc27bdc74e6e18340b5b01190128c85e1cc5
abdelhak ait elhad <aaitelhad@godeleats.com>
2022-12-29 17:43:10 +0100

    fix cors

--- config/cors.php
+++ config/cors.php
@@ -14,24 +14,12 @@ foreach (getallheaders() as $name => $value) {
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-     */
 
-    'paths' => ['api/*','graphql/*', 'sanctum/csrf-cookie', 'storage/*', 'api'],
+    'paths' => ['api/*', 'graphql', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [$origin],
 
     'allowed_origins_patterns' => [],
 
@@ -41,6 +29,6 @@ return [
 
     'max_age' => 2123123,
 
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 
 ];
