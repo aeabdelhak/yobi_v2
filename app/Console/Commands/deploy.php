6ef6099f9a9a84288a71e3ba48103267e00e249e
abdelhak ait elhad <aaitelhad@godeleats.com>
2022-12-29 18:48:21 +0100

    add deploy command

---
+++ app/Console/Commands/deploy.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class deploy extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'deploy';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'deploy';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        exec('
+        sudo chgrp -R www-data storage bootstrap/cache
+        sudo chmod -R ug+rwx storage bootstrap/cache
+        composer install --optimize-autoloader --no-dev
+        php artisan config:cache
+        php artisan route:cache');
+        return Command::SUCCESS;
+    }
+}
